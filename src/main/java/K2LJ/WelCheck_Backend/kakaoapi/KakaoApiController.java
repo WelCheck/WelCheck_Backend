f55f8629975ac64d629b17ef89dd0731a377771a
Anyeon00 <seluo65@naver.com>
2024-06-16 00:24:24 +0900

    240616

--- src/main/java/K2LJ/WelCheck_Backend/kakaoapi/KakaoApiController.java
+++ src/main/java/K2LJ/WelCheck_Backend/kakaoapi/KakaoApiController.java
@@ -3,6 +3,6 @@ package K2LJ.WelCheck_Backend.kakaoapi;
 import org.springframework.stereotype.Controller;
 
 public interface KakaoApiController {
-    //query받아서, 실제 주소를 반환하는 api
+    //검색어(query)를 전달받아서,검색된 실제주소 리스트를 응답하는 api
     public String searchAddress(String query);
 }
--- src/main/java/K2LJ/WelCheck_Backend/memberpackage/domain/member/WelfareWorkerMember.java
+++ src/main/java/K2LJ/WelCheck_Backend/memberpackage/domain/member/WelfareWorkerMember.java
@@ -1,6 +1,10 @@
 package K2LJ.WelCheck_Backend.memberpackage.domain.member;
 
+import K2LJ.WelCheck_Backend.facilitypackage.domain.Facility;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
@@ -11,6 +15,11 @@ public class WelfareWorkerMember extends Member{
     //변경필요
     private String workCertifed;    //재직증명서(또는 인증여부)
 
-    //변경필요
-    private String workSpace;   //근무지 수정필요 - address 상속
+    private String workSpace;
+/*
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "facility_id")
+    private Facility workSpace; //근무지
+*/
+
 }
